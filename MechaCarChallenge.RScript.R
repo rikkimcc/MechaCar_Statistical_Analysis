e3e6f7b2b47e0a8c3836208a2fc00151b5049e99
rikkimcc <89141436+rikkimcc@users.noreply.github.com>
2021-12-12 11:58:15 -0500

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+###### DELIVERABLE 1
+
 # read MechaCar_mpg file
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors =F)
 
@@ -8,3 +10,17 @@ lm(vehicle_length ~ mpg, MechaCar)
 
 # Perform linear regression summary
 summary(lm(vehicle_length ~ mpg, MechaCar))
+
+
+###### DELIVERABLE 2
+
+# read Suspension Coil csv
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors =F)
+
+# create total summary table
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard_Deviation=sd(PSI))
+
+# create lot summary table
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard_Deviation=sd(PSI), .groups = 'keep')
+
+
